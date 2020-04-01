8a2e71f1631cb94334fde837f9b55bdf61a27764
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-01 08:30:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,12 +11,10 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = Array[1, 2, 3, 4]
   array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = Array[1, 2, 3, 4]
   array.pop
 end
 
