a2360ffbf038d77cc29e9db6cc556723f29b76be
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-01 08:31:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,9 +1,9 @@
 def create_an_empty_array
-  []
+  emptyArray = []
 end
 
 def create_an_array
-  [1, 2, 3, 4]
+  newArray = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
@@ -23,7 +23,7 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array
+  array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
