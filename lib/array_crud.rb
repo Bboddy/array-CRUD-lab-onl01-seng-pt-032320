cd0adafeebb1bbec20e265db571a4a347d84c8f9
puckish-system-8282 <bryce.boddy@gmail.com>
2020-04-01 08:16:40 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  arry = Arry[]
+  arry.insert() "element"
 end
 
 def remove_element_from_end_of_array(array)
